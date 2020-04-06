9f7598d73fc2225625db18c4aa6db5d38c7f06e6
Valentin Berlin <valentin.berlin@protonmail.com>
2020-04-06 09:40:39 +0200

    add: events API basic tests

--- pages.tests/api.events.test.ts
+++ pages.tests/api.events.test.ts
@@ -1,8 +1,10 @@
 import { createMocks } from 'node-mocks-http';
 import { eventsEndpointHandler } from '../pages/api/events';
 
+type APIresponse = { _getStatusCode: () => any; _getData: () => any };
+
 describe('GET', () => {
-  let response: { _getStatusCode: () => any; _getData: () => any };
+  let response: APIresponse;
 
   beforeEach(() => {
     const { req, res } = createMocks({
@@ -12,11 +14,81 @@ describe('GET', () => {
     eventsEndpointHandler(req, res);
   });
 
-  test('returns 200', () => {
-    expect(response._getStatusCode()).toBe(200);
+  test('method allowed by API', () => {
+    expect(response._getStatusCode()).not.toBe(405);
   });
 
-  test('returns correct moto', () => {
-    expect(response._getData()).toBe(JSON.stringify({ moto: 'Akuna Matata' }));
+  test('returns an Array of objects', () => {
+    const dataObject = JSON.parse(response._getData());
+
+    expect(typeof dataObject).toBe('object');
+    expect(dataObject.length).toBeGreaterThanOrEqual(1);
+    expect(typeof dataObject[0]).toBe('object');
+  });
+});
+
+describe('POST', () => {
+  let response: APIresponse;
+
+  beforeEach(() => {
+    const { req, res } = createMocks({
+      method: 'POST',
+    });
+    response = res;
+    eventsEndpointHandler(req, res);
+  });
+
+  test('method allowed by API', () => {
+    expect(response._getStatusCode()).not.toBe(405);
+  });
+
+  test('returns non-empty string', () => {
+    const APIres = response._getData();
+    expect(typeof APIres).toBe('string');
+    expect(APIres.length).toBeGreaterThanOrEqual(1);
+  });
+});
+
+describe('DELETE', () => {
+  let response: APIresponse;
+
+  beforeEach(() => {
+    const { req, res } = createMocks({
+      method: 'DELETE',
+    });
+    response = res;
+    eventsEndpointHandler(req, res);
+  });
+
+  test('method allowed by API', () => {
+    expect(response._getStatusCode()).not.toBe(405);
+  });
+
+  test('returns deleted event Object', () => {
+    const APIres = JSON.parse(response._getData());
+    expect(typeof APIres).toBe('object');
+    expect(APIres.length).not.toBeDefined();
+  });
+});
+
+describe('PUT', () => {
+  let response: APIresponse;
+
+  beforeEach(() => {
+    const { req, res } = createMocks({
+      method: 'PUT',
+    });
+    response = res;
+    eventsEndpointHandler(req, res);
+  });
+
+  test('method allowed by API', () => {
+    expect(response._getStatusCode()).not.toBe(405);
+  });
+
+  test('returns non-empty string', () => {
+    const APIres = response._getData();
+    expect(typeof APIres).toBe('string');
+    expect(APIres.length).toBeGreaterThanOrEqual(1);
   });
 });
