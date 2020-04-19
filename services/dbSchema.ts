b08370d3294ce2a1d7952a7f5384221efe2e11c4
Valentin Berlin <valentin.berlin@protonmail.com>
2020-04-19 08:59:17 +0200

    add: database schema for agenda event

---
+++ services/dbSchema.ts
@@ -0,0 +1,35 @@
+export interface AgendaEvent {
+  name: string;
+  start: Date;
+  end: Date;
+  state: AgendaEventState;
+}
+
+type AgendaEventState = {
+  [category in AgendaEventCategory]?: AgendaEventStatus;
+};
+
+type AgendaEventCategory =
+  | 'home'
+  | 'current'
+  | 'exhibitions'
+  | 'theatreanddance'
+  | 'music'
+  | 'sports'
+  | 'fairs'
+  | 'events'
+  | 'christmas';
+
+type AgendaEventStatus = 'candidate' | 'highlight' | 'mainfocus';
+
+// this is an examle of agenda event object
+export const exampleAgendaEvent: AgendaEvent = {
+  name: 'PhotoEspaña 2020',
+  start: new Date('1/4/2020'),
+  end: new Date('24/5/2020'),
+  state: {
+    home: 'candidate',
+    current: 'highlight',
+    fairs: 'mainfocus',
+  },
+};
