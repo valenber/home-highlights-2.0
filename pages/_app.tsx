a652e715c004c2b8875771922dfa4c64fe44d8e1
Valentin Berlin <valentin.berlin@protonmail.com>
2022-09-13 19:03:18 +0200

    run preset-safe codemod on pages folder
    https://mui.com/material-ui/migration/migration-v4/#preset-safe

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,10 +9,24 @@ import { ReactText, StrictMode } from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store';
 import React from 'react';
-import { Button, createTheme, ThemeProvider } from '@material-ui/core';
-import { green, purple } from '@material-ui/core/colors';
+import {
+  Button,
+  createTheme,
+  ThemeProvider,
+  Theme,
+  StyledEngineProvider,
+  adaptV4Theme,
+} from '@mui/material';
+import { green, purple } from '@mui/material/colors';
 
-const customTheme = createTheme({
+
+declare module '@mui/styles/defaultTheme' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface DefaultTheme extends Theme {}
+}
+
+
+const customTheme = createTheme(adaptV4Theme({
   palette: {
     primary: {
       main: purple[500],
@@ -21,7 +35,7 @@ const customTheme = createTheme({
       main: green[500],
     },
   },
-});
+}));
 
 const AppRoot: React.FC<AppProps> = ({ Component, pageProps }) => {
   const notistackRef = React.createRef();
@@ -34,27 +48,29 @@ const AppRoot: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <StrictMode>
       <Provider store={store}>
-        <ThemeProvider theme={customTheme}>
-          <SnackbarProvider
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            ref={notistackRef}
-            maxSnack={3}
-            action={(key) => (
-              <Button
-                size="small"
-                variant="outlined"
-                onClick={onClickDismiss(key)}
-              >
-                dismiss
-              </Button>
-            )}
-          >
-            <div className="appLayout">
-              <Component {...pageProps} />
-            </div>
-          </SnackbarProvider>
-        </ThemeProvider>
+        <StyledEngineProvider injectFirst>
+          <ThemeProvider theme={customTheme}>
+            <SnackbarProvider
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              ref={notistackRef}
+              maxSnack={3}
+              action={(key) => (
+                <Button
+                  size="small"
+                  variant="outlined"
+                  onClick={onClickDismiss(key)}
+                >
+                  dismiss
+                </Button>
+              )}
+            >
+              <div className="appLayout">
+                <Component {...pageProps} />
+              </div>
+            </SnackbarProvider>
+          </ThemeProvider>
+        </StyledEngineProvider>
       </Provider>
     </StrictMode>
   );
