dd1ba578ab802da5569a5ab4b2a638bbb9d10f6b
Valentin Berlin <valentin.berlin@protonmail.com>
2022-09-22 14:54:33 +0200

    switch to matched event on click result button

--- features/event-search/MatchedEventButton.tsx
+++ features/event-search/MatchedEventButton.tsx
@@ -1,7 +1,19 @@
-import { Chip } from '@mui/material';
+import { Button } from '@mui/material';
 import React, { FC } from 'react';
+import { useAppDispatch } from 'store';
+import { selectEventToEdit } from 'store/editorSlice';
 import { SearchResult } from './eventsMatcher';
 
 export const MatchedEventButton: FC<{ event: SearchResult }> = ({ event }) => {
-  return <Chip label={event.name} variant="outlined" size="small" />;
+  const dispatch = useAppDispatch();
+
+  function editMatchedEvent() {
+    dispatch(selectEventToEdit(event.id));
+  }
+
+  return (
+    <Button variant="outlined" size="small" onClick={editMatchedEvent}>
+      {event.name}
+    </Button>
+  );
 };
